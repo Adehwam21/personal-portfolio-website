b0eb388de4358ac4d017b4183a158a4d80171483
Adehwam21 <drewaaron96@gmail.com>
2025-01-17 20:34:26 +0000

    chore: add fonts

--- portfolio-frontend/tailwind.config.ts
+++ portfolio-frontend/tailwind.config.ts
@@ -9,6 +9,19 @@ const config: Config = {
         background: 'var(--background-color)',
         text: 'var(--text-color)',
       },
+      height: {
+        '100': "31rem",
+        '102': "32rem",
+        '105': "33.5rem",
+        '110': "43rem",
+        '111': "45rem",
+      },
+      
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        fira: ['Fira Code', 'monospace'],
+        scode: ['Source Code Pro', 'monospace'],
+      },
     },
   },
   daisyui: {
