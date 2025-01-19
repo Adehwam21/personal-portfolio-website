38a16df153c1fd9e9ff99eb1bbc107d4e9e3a0af
Adehwam21 <drewaaron96@gmail.com>
2025-01-19 03:54:03 +0000

    feat: add logo to navbar

--- portfolio-frontend/src/components/Navbar.tsx
+++ portfolio-frontend/src/components/Navbar.tsx
@@ -28,17 +28,25 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="fixed w-full flex text-base top-0 right-0 z-50 font-bold justify-between bg-white shadow-lg px-10 h-20 items-center dark:bg-slate-800">
+    <nav className="fixed w-full flex text-base top-0 right-0 z-50 font-bold justify-between bg-white shadow-lg px-4 md:px-10 h-20 items-center dark:bg-slate-800">
       {/* Left Section */}
-      <div className="flex space-x-4 items-center">
-        {/* Brand Name */}
-        <Link to="#home-hero-section" className="text-sm font-bold px-4 py-2 rounded-md btn-ghost">
-          AARON KUDADJIE
-        </Link>
+      <div className="flex row space-x-2 items-center">
+
+        {/* Logo */}
+        <div className="flex btn-ghost rounded-md items-center space-x-2">
+          <Link to="#home-hero-section" className="flex items-center space-x-2 p-2">
+            <img
+              src="images/logo.webp"
+              alt="logo"
+              className="w-10 h-10 object-cover rounded-full border-4- shadow-lg"
+            />
+            <span className="text-sm font-bold">AARON KUDADJIE</span>
+          </Link>
+        </div>
 
         {/* Theme Toggle Button */}
         <Button
-          className={`btn btn-ghost rounded-full text-base`}
+          className={`btn btn-ghost rounded-full text-2xl`}
           onClick={toggleTheme}
         >
           {theme === "light" ? <AiOutlineMoon /> : <AiOutlineSun />}
@@ -61,7 +69,7 @@ const Navbar: React.FC = () => {
 
       {/* Hamburger Menu Button (Visible on Small Screens) */}
       <Button
-        className={`btn btn-ghost rounded-full text-base md:hidden`}
+        className={`btn btn-ghost rounded-full text-2xl md:hidden`}
         onClick={toggleNavMenu}
       >
         {isMenuOpen ? <AiOutlineClose /> : <RxHamburgerMenu />}
@@ -80,7 +88,7 @@ const Navbar: React.FC = () => {
               <li key={link.href} className="w-full">
                 <Link
                   to={link.href}
-                  className="block text-end text-sm w-full h-8 px-14 pt-8 pb-12 border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition-all"
+                  className="block text-end text-sm w-full h-8 px-8 pt-8 pb-12 border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition-all"
                 >
                   {link.label}
                 </Link>
