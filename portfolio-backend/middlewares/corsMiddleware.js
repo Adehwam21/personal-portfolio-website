4f5d2f14874e70f8c55bd3cc97e028599b07381d
Adehwam21 <drewaaron96@gmail.com>
2025-01-27 14:50:21 +0000

    fix: contact form

--- portfolio-backend/middlewares/corsMiddleware.js
+++ portfolio-backend/middlewares/corsMiddleware.js
@@ -1,8 +1,15 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const frontEndUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 const corsMiddleware = cors({
-  origin: ['http://localhost:3000', 'http://192.168.43.49:3000'],
+  origin: [
+    'http://localhost:3000', 
+    'http://192.168.43.49:3000', 
+    'https://aaronkudadjie.vercel.app',
+    'http://localhost:5173',
+    frontEndUrl
+  ]
 });
 
 const urlencodedMiddleware = bodyParser.urlencoded({ extended: false });
